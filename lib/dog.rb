63fef27497a93a7bdda09783d9559dd77a95187b
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-14 18:54:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    def initialize(dogs_name, breed="Mutt")
+        @name = dogs_name
+        @breed = breed
+    end
+    def dogs_name
+        @name
+    end
+    def breed
+        @breed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    def initialize(persons_name)
+        @name = persons_name
+    end
+    def persons_name
+        @name
+    end
+end
